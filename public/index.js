2c07a25d77b846cf11f132412f82e2a27ee6386f
sammit badodekar <badodekarsammit@gmail.com>
2023-04-10 16:27:43 +0530

    added comics and events on search box

--- public/index.js
+++ public/index.js
@@ -1,134 +1,214 @@
-setTimeout(()=>{
-    document.getElementById('preloader').style.display = 'none';  
-    },2000)
+setTimeout(() => {
+  document.getElementById("preloader").style.display = "none";
+}, 2000);
+function scrollToSection(id) {
+  var element = document.getElementById(id);
+  element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
 
-document.addEventListener('click', function(event) {
-    if (event.target.matches('a') || event.target.matches('img') || event.target.matches('h4')) {
-      document.getElementById('preloader').style.display = 'flex';
-      setTimeout(()=>{
-      document.getElementById('preloader').style.display = 'none';  
-      },5000)
-    }
-  });
+document.addEventListener("click", function (event) {
+  if (
+    event.target.matches("a") ||
+    event.target.matches("img") ||
+    event.target.matches("h4")
+  ) {
+    document.getElementById("preloader").style.display = "flex";
+    setTimeout(() => {
+      document.getElementById("preloader").style.display = "none";
+    }, 5000);
+  }
+});
 
 /* search inputs and results */
 let searchBox = document.getElementById("search-box");
-let searchResults = document.getElementById("search-result")
-searchBox.addEventListener("input", async ()=>{
-    searchResultsHtml.style.display = 'grid'
-    setTimeout(async() => {
-    let searchInputs = searchBox.value
-    if (searchInputs === ""){
-        searchResults.innerHTML = ""
-    }else{
-        searchResults.innerHTML = "Searching..."
+let searchResults = document.getElementById("search-result");
+searchBox.addEventListener("input", async () => {
+  searchResultsHtml.style.display = "grid";
+    let searchInputs = searchBox.value;
+    if (searchInputs === "") {
+      searchResults.innerHTML = "";
+    } else {
+      searchResults.innerHTML = "Searching...";
     }
-    let rawCharNameData = await fetch(`/characters?query=${searchInputs}`)
-    let charNameData = await rawCharNameData.json()
+    searchResults.innerHTML = "";
+    let rawCharNameData = await fetch(`/characters?query=${searchInputs}`);
+    let charNameData = await rawCharNameData.json();
     const charNames = Object.entries(charNameData);
-    searchResults.innerHTML = ""
-    charNames.forEach(element => {
-       searchResults.innerHTML += `<p><a href="/character-info?query=${element[1].id}">${element[1].name}</a></p>`
-    }); 
-      },1000)
-})
-
+    for(i=1;i<6;i++){
+        searchResults.innerHTML += `<p><a href="/character-info?query=${charNames[i][1].id}">${charNames[i][1].name}</a></p>`;
+    }
+    let rawComicsNameData = await fetch(`/comics?query=${searchInputs}`);
+    let comicsNameData = await rawComicsNameData.json();
+    const comNames = Object.entries(comicsNameData);
+    for(i=1;i<6;i++){
+        searchResults.innerHTML += `<p><a href="/comics-info?query=${comNames[i][1].id}">${comNames[i][1].title}</a></p>`;
+    }
+    let rawEventsNameData = await fetch(`/events?query=${searchInputs}`);
+    let EventsNameData = await rawEventsNameData.json();
+    const EventsNames = Object.entries(EventsNameData);
+    for(i=1;i<6;i++){
+        searchResults.innerHTML += `<p><a href="/events-info?query=${EventsNames[i][1].title}">${EventsNames[i][1].title}</a></p>`;
+    }
+});
 
 let characterContent = document.getElementById("character");
-async function randomCaracters(){
-    let rawRandonChar = await fetch(`/random-characters?query=${getRandomCharacters()}`);
-    let charNameData = await rawRandonChar.json()
-    const charNames = Object.entries(charNameData);
-    for(i=0;i<30;i++) {
-        if(charNames[i][1].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available'|| charNames[i][1].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708'){
-            continue;
-        }
-        characterContent.innerHTML += `<a href="/character-info?query=${charNames[i][1].id}" id="characters">
+async function randomCaracters() {
+  let rawRandonChar = await fetch(
+    `/random-characters?query=${getRandomCharacters()}`
+  );
+  let charNameData = await rawRandonChar.json();
+  const charNames = Object.entries(charNameData);
+  for (i = 0; i < 30; i++) {
+    if (
+      charNames[i][1].thumbnail.path ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" ||
+      charNames[i][1].thumbnail.path ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708"
+    ) {
+      continue;
+    }
+    characterContent.innerHTML += `<a href="/character-info?query=${charNames[i][1].id}" id="characters">
         <img src="${charNames[i][1].thumbnail.path}.${charNames[i][1].thumbnail.extension}" alt="">
-        <h4>${charNames[i][1].name}</h4></a>`
-    };
+        <h4>${charNames[i][1].name}</h4></a>`;
+  }
 }
 randomCaracters();
 
 /* For generating random comics */
 let comicContent = document.getElementById("comics");
-async function randomComics(){
-    let rawRandonChar = await fetch(`/random-comics?query=${getRandomComics()}`);
-    let charNameData = await rawRandonChar.json()
-    const charNames = Object.entries(charNameData);
-    for(i=0;i<30;i++) {
-        if(charNames[i][1].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available'|| charNames[i][1].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708'){
-            continue;
-        }
-        comicContent.innerHTML += `<a href="/comics-info?query=${charNames[i][1].id}" id="characters">
+async function randomComics() {
+  let rawRandonChar = await fetch(`/random-comics?query=${getRandomComics()}`);
+  let charNameData = await rawRandonChar.json();
+  const charNames = Object.entries(charNameData);
+  for (i = 0; i < 30; i++) {
+    if (
+      charNames[i][1].thumbnail.path ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" ||
+      charNames[i][1].thumbnail.path ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708"
+    ) {
+      continue;
+    }
+    comicContent.innerHTML += `<a href="/comics-info?query=${charNames[i][1].id}" id="characters">
         <img src="${charNames[i][1].thumbnail.path}.${charNames[i][1].thumbnail.extension}" alt="">
-        <h4>${charNames[i][1].title}</h4></a>`
-    };
+        <h4>${charNames[i][1].title}</h4></a>`;
+  }
 }
 randomComics();
 /* For generating random events */
 let eventsContent = document.getElementById("events");
-async function randomEvents(){
-    let rawRandomEvents = await fetch(`/random-events?query=${getRandomEvents()}`);
-    let eventsNameData = await rawRandomEvents.json()
-    const charNames = Object.entries(eventsNameData);
-    for(i=0;i<30;i++) {
-        if(charNames[i][1].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available'|| charNames[i][1].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708'){
-            continue;
-        }
-        eventsContent.innerHTML += `<a href="/events-info?query=${charNames[i][1].title}" id="characters">
+async function randomEvents() {
+  let rawRandomEvents = await fetch(
+    `/random-events?query=${getRandomEvents()}`
+  );
+  let eventsNameData = await rawRandomEvents.json();
+  const charNames = Object.entries(eventsNameData);
+  for (i = 0; i < 30; i++) {
+    if (
+      charNames[i][1].thumbnail.path ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" ||
+      charNames[i][1].thumbnail.path ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708"
+    ) {
+      continue;
+    }
+    eventsContent.innerHTML += `<a href="/events-info?query=${charNames[i][1].title}" id="characters">
         <img src="${charNames[i][1].thumbnail.path}.${charNames[i][1].thumbnail.extension}" alt="">
-        <h4>${charNames[i][1].title}</h4></a>`
-    };
+        <h4>${charNames[i][1].title}</h4></a>`;
+  }
 }
 randomEvents();
-const movies = ["Iron man","The Incredible Hulk","Iron Man 2","Thor","Captain America: The First Avenger","The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier","Guardians of the Galaxy","Avengers: Age of Ultron","Captain America: Civil War","Doctor Strange","Guardians of the Galaxy Vol. 2","Spider-Man Homecoming","Thor: Ragnarok","Black Panther","Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame","Spider-Man: Far from Home","Black Widow","Shang-Chi and the Legend of the Ten Rings","Eternals","Spider-Man: No Way Home","Doctor Strange in the Multiverse of Madness","Thor: Love and Thunder","Black panther Wakanda Forever","Ant-Man and the Wasp: Quantumania"]
-const tvShows = ["Wandavision","The Falcon and the Winter Soldier","Loki","What if...?","Hawkeye","Moon Knight",'Ms Marvel',"I Am Groot","The Guardians of the Galaxy: Holiday Special"]
-let moviesContainer = document.getElementById("movies")
-let tvShowsContainer = document.getElementById("tv-shows")
-movies.forEach(element => {
-    async function moviesData(){
-    let rawCharNameData = await fetch(`https://www.omdbapi.com/?t=${element}&apikey=a664b916`)
-    let charNameData = await rawCharNameData.json()
-    let moviesInfo = Object.values(charNameData) 
-    searchResults.innerHTML = ""
+const movies = [
+  "Iron man",
+  "The Incredible Hulk",
+  "Iron Man 2",
+  "Thor",
+  "Captain America: The First Avenger",
+  "The Avengers",
+  "Iron Man 3",
+  "Thor: The Dark World",
+  "Captain America: The Winter Soldier",
+  "Guardians of the Galaxy",
+  "Avengers: Age of Ultron",
+  "Captain America: Civil War",
+  "Doctor Strange",
+  "Guardians of the Galaxy Vol. 2",
+  "Spider-Man Homecoming",
+  "Thor: Ragnarok",
+  "Black Panther",
+  "Avengers: Infinity War",
+  "Ant-Man and the Wasp",
+  "Captain Marvel",
+  "Avengers: Endgame",
+  "Spider-Man: Far from Home",
+  "Black Widow",
+  "Shang-Chi and the Legend of the Ten Rings",
+  "Eternals",
+  "Spider-Man: No Way Home",
+  "Doctor Strange in the Multiverse of Madness",
+  "Thor: Love and Thunder",
+  "Black panther Wakanda Forever",
+  "Ant-Man and the Wasp: Quantumania",
+];
+const tvShows = [
+  "Wandavision",
+  "The Falcon and the Winter Soldier",
+  "Loki",
+  "What if...?",
+  "Hawkeye",
+  "Moon Knight",
+  "Ms Marvel",
+  "I Am Groot",
+  "The Guardians of the Galaxy: Holiday Special",
+];
+let moviesContainer = document.getElementById("movies");
+let tvShowsContainer = document.getElementById("tv-shows");
+movies.forEach((element) => {
+  async function moviesData() {
+    let rawCharNameData = await fetch(
+      `https://www.omdbapi.com/?t=${element}&apikey=a664b916`
+    );
+    let charNameData = await rawCharNameData.json();
+    let moviesInfo = Object.values(charNameData);
+    searchResults.innerHTML = "";
     moviesContainer.innerHTML += `
     <a href="/movies-info?query=${moviesInfo[0]}">
     <img src="${moviesInfo[13]}" alt="" class="movie-img">
     <p>${moviesInfo[0]}</p>
-    </a>`
-    }
-    moviesData();
+    </a>`;
+  }
+  moviesData();
 });
-tvShows.forEach(element => {
-    async function moviesData(){
-    let rawCharNameData = await fetch(`https://www.omdbapi.com/?t=${element}&apikey=a664b916`)
-    let charNameData = await rawCharNameData.json()
-    let moviesInfo = Object.values(charNameData) 
-    searchResults.innerHTML = ""
+tvShows.forEach((element) => {
+  async function moviesData() {
+    let rawCharNameData = await fetch(
+      `https://www.omdbapi.com/?t=${element}&apikey=a664b916`
+    );
+    let charNameData = await rawCharNameData.json();
+    let moviesInfo = Object.values(charNameData);
+    searchResults.innerHTML = "";
     tvShowsContainer.innerHTML += `
     <a href="/movies-info?query=${moviesInfo[0]}">
     <img src="${moviesInfo[13]}" alt="" class="movie-img">
     <p>${moviesInfo[0]}</p>
-    </a>`
-    }
-    moviesData();
+    </a>`;
+  }
+  moviesData();
 });
 
-
 /* loading animation */
-let body = document.querySelector("body")
-let searchResultsHtml = document.getElementById("search-result")
-body.addEventListener("click",()=>{
-    searchResultsHtml.style.display = 'none'
-})
+let body = document.querySelector("body");
+let searchResultsHtml = document.getElementById("search-result");
+body.addEventListener("click", () => {
+  searchResultsHtml.style.display = "none";
+});
 /* functions for giving offsets for characters/comics/events */
-function getRandomCharacters(){
-    return Math.floor(Math.random() * 1500);
+function getRandomCharacters() {
+  return Math.floor(Math.random() * 1500);
 }
-function getRandomComics(){
-    return Math.floor(Math.random() * 40000);
+function getRandomComics() {
+  return Math.floor(Math.random() * 40000);
 }
-function getRandomEvents(){
-    return Math.floor(Math.random() * 72);
+function getRandomEvents() {
+  return Math.floor(Math.random() * 72);
 }
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const { send } = require('express/lib/response');
+const express = require("express");
+const { send } = require("express/lib/response");
 const app = express();
 const axios = require("axios");
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
 
-app.get('/'),(req, res) => {
-    res.send('index.html')
-}
-
+app.get("/"),
+  (req, res) => {
+    res.send("index.html");
+  };
 
 /* search box and results*/
 
-var Marvel = require('marvel')
-var marvel = new Marvel({ publicKey: "2ab9cbe8b87244a8877154fea3e3c60b", privateKey: "0e44fa45a253873efe3c63f2a923e9e8c26be183"})
+var Marvel = require("marvel");
+var marvel = new Marvel({
+  publicKey: "6026d2deba1899c72c1c66e9393a78a8",
+  privateKey: "46731369c3e6312d39228bc1b9a4b4dd80c948af",
+});
 
-app.get('/characters', (req, res) => {
+app.get("/characters", (req, res) => {
   const query = req.query.query;
-  marvel.characters.nameStartsWith(`${query}`).get((err, resp) => {
+  marvel.characters.nameStartsWith(`${query}`).get(async (err, resp) => {
     if (err) {
       console.log("Error retrieving data:", err);
       res.status(500).send("An error occurred while retrieving data");
@@ -29,8 +32,30 @@ app.get('/characters', (req, res) => {
     }
   });
 });
+app.get("/comics", (req, res) => {
+  const query = req.query.query;
+  marvel.comics.titleStartsWith(`${query}`).get((err, resp) => {
+    if (err) {
+      console.log("Error retrieving data:", err);
+      reject(err);
+    } else {
+      res.send(resp);
+    }
+  });
+});
+app.get("/events", (req, res) => {
+  const query = req.query.query;
+  marvel.events.nameStartsWith(`${query}`).get((err, resp) => {
+    if (err) {
+      console.log("Error retrieving data:", err);
+      reject(err);
+    } else {
+      res.send(resp)
+    }
+  });
+});
 /* generating random characters */
-app.get('/random-characters', (req, res) => {
+app.get("/random-characters", (req, res) => {
   const query = req.query.query;
   marvel.characters.offset(`${query}`).get((err, resp) => {
     if (err) {
@@ -42,7 +67,7 @@ app.get('/random-characters', (req, res) => {
   });
 });
 /* generating random comics */
-app.get('/random-comics', (req, res) => {
+app.get("/random-comics", (req, res) => {
   const query = req.query.query;
   marvel.comics.offset(`${query}`).get((err, resp) => {
     if (err) {
@@ -55,7 +80,7 @@ app.get('/random-comics', (req, res) => {
 });
 
 /* generating random events */
-app.get('/random-events', (req, res) => {
+app.get("/random-events", (req, res) => {
   const query = req.query.query;
   marvel.events.offset(`${query}`).get((err, resp) => {
     if (err) {
@@ -69,86 +94,31 @@ app.get('/random-events', (req, res) => {
 /* when user clicks on search results */
 /* for getting char img,name,descriptions */
 let charInfo;
-app.get('/character-info',(req,res)=>{
+app.get("/character-info", (req, res) => {
   const query = req.query.query;
-   marvel.characters.id(`${query}`).get(async(err, resp) => {
+  marvel.characters.id(`${query}`).get(async (err, resp) => {
     if (err) {
       console.log("Error retrieving data:", err);
       res.status(500).send("An error occurred while retrieving data");
-      
     } else {
       charInfo = resp[0];
       let comicsData = [];
       const comicData = await comicNameInfo(query);
-      let eventName = resp[0].events.items
-      let comData = Object.values(comicData)
-      comData.forEach(element => {
-       comicsData.push(element) 
+      let eventName = resp[0].events.items;
+      let comData = Object.values(comicData);
+      comData.forEach((element) => {
+        comicsData.push(element);
       });
-      res.render('character-info',{
-        charImg:`${charInfo.thumbnail.path}.${charInfo.thumbnail.extension}`,
-        charName:`${charInfo.name}`,
-        charDesc:`${charInfo.description}`,
-        comName:`${JSON.stringify(comicsData)}`,
-        eveName:`${JSON.stringify(eventName)}`
-      })
+      res.render("character-info", {
+        charImg: `${charInfo.thumbnail.path}.${charInfo.thumbnail.extension}`,
+        charName: `${charInfo.name}`,
+        charDesc: `${charInfo.description}`,
+        comName: `${JSON.stringify(comicsData)}`,
+        eveName: `${JSON.stringify(eventName)}`,
+      });
     }
   });
-})
-let comInfo;
-app.get('/comics-info',(req,res)=>{
-  const query = req.query.query;
-   marvel.comics.id(`${query}`).get(async(err, resp) => {
-    if (err) {
-      console.log("Error retrieving data:", err);
-      res.status(500).send("An error occurred while retrieving data");
-    } else {
-      comInfo = resp[0];
-      res.render('comics-info',{
-        comicImg:`${comInfo.thumbnail.path}.${comInfo.thumbnail.extension}`,
-        comicName:`${comInfo.title}`,
-        comicDesc:`${comInfo.description}`
-      })
-    }
-  });
-})
-app.get('/events-info',(req,res)=>{
-  const query = req.query.query;
-   marvel.events.name(`${query}`).get(async(err, resp) => {
-    if (err) {
-      console.log("Error retrieving data:", err);
-      res.status(500).send("An error occurred while retrieving data");
-    } else {
-      eveInfo = resp[0];
-      res.render('events-info',{
-        eventsImg:`${eveInfo.thumbnail.path}.${eveInfo.thumbnail.extension}`,
-        eventsName:`${eveInfo.title}`,
-        eventsDesc:`${eveInfo.description}`,
-        prev:`${eveInfo.previous.name}`,
-        next:`${eveInfo.next.name}`
-      })
-    }
-  });
-})
-app.get('/movies-info',async(req,res)=>{
-  const query = req.query.query;
-  const options = {
-    method: 'GET',
-    url: `https://www.omdbapi.com/?t=${query}&apikey=a664b916`
-  };
-  
-      const RawMoviesData = await axios.request(options)
-      res.render('movies-info',{
-        movieImg:`${RawMoviesData.data.Poster}`,
-        movieName:`${RawMoviesData.data.Title}`,
-        movieCast:`${RawMoviesData.data.Actors}`,
-        movieDirector:`${RawMoviesData.data.Director}`,
-        movieDesc:`${RawMoviesData.data.Plot}`,
-        movieRuntime:`${RawMoviesData.data.Runtime}`,
-        movieRating:`${RawMoviesData.data.imdbRating}`,
-        movieBoxOffice:`${RawMoviesData.data.BoxOffice}`
-      }) 
-    })
+});
 /*for getting comic img,name,descriptions in which character has appeared*/
 function comicNameInfo(charId) {
   return new Promise((resolve, reject) => {
@@ -157,10 +127,63 @@ function comicNameInfo(charId) {
         console.log("Error retrieving data:", err);
         reject(err);
       } else {
-        let comicData = resp
+        let comicData = resp;
         resolve(comicData);
       }
     });
   });
 }
+let comInfo;
+app.get("/comics-info", (req, res) => {
+  const query = req.query.query;
+  marvel.comics.id(`${query}`).get(async (err, resp) => {
+    if (err) {
+      console.log("Error retrieving data:", err);
+      res.status(500).send("An error occurred while retrieving data");
+    } else {
+      comInfo = resp[0];
+      res.render("comics-info", {
+        comicImg: `${comInfo.thumbnail.path}.${comInfo.thumbnail.extension}`,
+        comicName: `${comInfo.title}`,
+        comicDesc: `${comInfo.description}`,
+      });
+    }
+  });
+});
+app.get("/events-info", (req, res) => {
+  const query = req.query.query;
+  marvel.events.name(`${query}`).get(async (err, resp) => {
+    if (err) {
+      console.log("Error retrieving data:", err);
+      res.status(500).send("An error occurred while retrieving data");
+    } else {
+      eveInfo = resp[0];
+      res.render("events-info", {
+        eventsImg: `${eveInfo.thumbnail.path}.${eveInfo.thumbnail.extension}`,
+        eventsName: `${eveInfo.title}`,
+        eventsDesc: `${eveInfo.description}`,
+        prev: `${eveInfo.previous.name}`,
+        next: `${eveInfo.next.name}`,
+      });
+    }
+  });
+});
+app.get("/movies-info", async (req, res) => {
+  const query = req.query.query;
+  const options = {
+    method: "GET",
+    url: `https://www.omdbapi.com/?t=${query}&apikey=a664b916`,
+  };
 
+  const RawMoviesData = await axios.request(options);
+  res.render("movies-info", {
+    movieImg: `${RawMoviesData.data.Poster}`,
+    movieName: `${RawMoviesData.data.Title}`,
+    movieCast: `${RawMoviesData.data.Actors}`,
+    movieDirector: `${RawMoviesData.data.Director}`,
+    movieDesc: `${RawMoviesData.data.Plot}`,
+    movieRuntime: `${RawMoviesData.data.Runtime}`,
+    movieRating: `${RawMoviesData.data.imdbRating}`,
+    movieBoxOffice: `${RawMoviesData.data.BoxOffice}`,
+  });
+});
